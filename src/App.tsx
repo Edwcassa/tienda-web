65b2fc2c34032c046b80341cb8a8a967bc4aaca9
Edwcassa <edwar.cassa@gmail.com>
2022-12-18 19:31:46 -0500

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      
+      <p className="text-3xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
